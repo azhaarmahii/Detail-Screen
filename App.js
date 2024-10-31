c64d3452ac65e3d0bff34aba665cf4a5c732f7b0
azhaarmahii <azhaarmahii88@gmail.com>
2024-10-31 08:14:49 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Detail from "./screen/Detail";
 import HomeScreen from "./screen/HomeScreen";
+import webView from "./screen/WebView";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}initialRouteName="Home">
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="WebView"
+      >
         <Stack.Screen name="Detail" component={Detail} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="WebView" component={webView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
