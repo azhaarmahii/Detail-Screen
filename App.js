e883743eb63235c3da3f95f4eceb66a6f587fb55
azhaarmahii <azhaarmahii88@gmail.com>
2024-10-31 07:37:27 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Detail from "./screen/auth/Detail";
+import Detail from "./screen/Detail";
+import HomeScreen from "./screen/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-     <Stack.Navigator screenOptions={{headerShown:false}}>
-       <Stack.Screen name="Detail" component={Detail}/>
-     </Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}initialRouteName="Home">
+        <Stack.Screen name="Detail" component={Detail} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
